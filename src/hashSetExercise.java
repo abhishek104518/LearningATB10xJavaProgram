779ef9c851c1fbdebfa7ac9362e53b99f884d1ad
ABHISHEK <abhispujari@gmail.com>
2025-02-03 18:43:14 +0530

    3rd Feb

---
+++ src/hashSetExercise.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class hashSetExercise {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        HashSet<String> hs = new HashSet<String>();
+        hs.add("USA");
+        hs.add("UK");
+        hs.add("India");
+        hs.add("Abhishek");
+        hs.add("Pujari");
+        System.out.println(hs);
+
+        Iterator<String> i = hs.iterator();
+        // System.out.println(i.next());
+
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+    }
+
+}
