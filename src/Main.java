85d46c91460c81ae4308f4bbee5d78a04ec7efe6
ABHISHEK <abhispujari@gmail.com>
2025-02-03 19:22:49 +0530

    3rd Feb

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.println("\t");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main{
+    public static void main(String args[]){
+        int[] arr = {4,9,23,12,57};
+        int sum = 0;
+        for(int i : arr)
+            sum += i;
+        System.out.println("the sum of arr is : "+ sum);
     }
 }
