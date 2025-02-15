2cabb709144f81fdc35c63e89186a232f8fa11ba
ABHISHEK <abhispujari@gmail.com>
2025-02-15 19:59:36 +0530

    15th_Feb

--- src/Task_5th_Feb/lab01_StringFunctions.java
+++ src/Task_5th_Feb/lab01_StringFunctions.java
@@ -13,10 +13,12 @@ public class lab01_StringFunctions {
 
     public void reverseString(){
         String str = "Abhishek Pujari";
-        String reverse = "";
+       // String reverse = "";
         for(int i =str.length()-1; i>=0; i--){
-            reverse += str.charAt(i);
+            //reverse += str.charAt(i);
+            System.out.print(str.charAt(i));
         }
-        System.out.println(reverse);
+        System.out.println();
+        //System.out.println(reverse);
     }
 }
